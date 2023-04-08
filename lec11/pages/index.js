841bb923f10c33426721c66ed828e9579c3e0d97
Summiayazaheer <118471587+Summiayazaheer@users.noreply.github.com>
2023-04-08 13:24:49 +0500

    first commit

---
+++ lec11/pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div className='m-h-screen bg-gray-300 flex flex-col items-center justify-center'>
+      <div>
+        <img src="/assets/event2.jpeg"
+        className='w-[150px] h-[150px]'/>
+        <div className='p-5'>
+          <h2 className='font-bold text-lg'>Programming for everyone</h2>
+          <h3>May 12,2021</h3>
+          <p>lorem</p>
+
+        </div>
+      </div>
+      index
+    </div>
+  )
+}
+
+export default index
